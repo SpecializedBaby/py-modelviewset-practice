70df4c5064feb12e7dec3cd52200b31a667fa570
Dede <specialized8393@gmail.com>
2024-06-07 14:59:47 +0100

    feat: add default router for author view set with basename author

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,11 @@
-# Create your urls here
+from rest_framework import routers
 
-urlpatterns = []
+from .views import AuthorViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r"authors", AuthorViewSet, basename="author")
+
+urlpatterns = router.urls
+
+app_name = "author"
